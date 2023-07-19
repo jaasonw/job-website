1969d0af37625d65575591e25bdc5f2b3e3c79e8
jaasonw <jayywong92@gmail.com>
2023-07-19 03:58:29 -0700

    fix test cases

--- tests/test_get_years_of_experience.py
+++ tests/test_get_years_of_experience.py
@@ -14,7 +14,7 @@ def test_get_years_of_experience():
             Strong written and verbal communication
             """
         )
-        == "5"
+        == 5
     )
     assert (
         get_years_of_experience(
@@ -27,7 +27,7 @@ def test_get_years_of_experience():
             Proven ability to write clean, clear and commented code 
             """
         )
-        == "3"
+        == 3
     )
     assert (
         get_years_of_experience(
@@ -45,7 +45,7 @@ def test_get_years_of_experience():
             bottlenecks
             """
         )
-        == "2, 2"
+        == 2
     )
     assert (
         get_years_of_experience(
@@ -59,5 +59,5 @@ def test_get_years_of_experience():
             A willingness to learn new frameworks and technologies as needed
             """
         )
-        == "2, 2, 1"
+        == 2
     )
