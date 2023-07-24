ee0850560295ac84c71139ec08a02ad5668db732
jaasonw <jayywong92@gmail.com>
2023-07-23 23:30:40 -0700

    jk i dont want to fix it rn

--- frontend/components/updated.tsx
+++ frontend/components/updated.tsx
@@ -1,23 +1,24 @@
-async function getTime() {
-  const resp = await fetch(
-    "http://worldtimeapi.org/api/timezone/America/Los_Angeles",
-    {
-      next: {
-        tags: ["jobs"],
-        revalidate: 0,
-      },
-    },
-  );
-  const json = await resp.json();
-  const time = new Date(json.datetime);
-  return time.toLocaleString("en-US", { timeZone: "PST" }) + " PST";
-}
+// async function getTime() {
+//   const resp = await fetch(
+//     "http://worldtimeapi.org/api/timezone/America/Los_Angeles",
+//     {
+//       next: {
+//         tags: ["jobs"],
+//         revalidate: 0,
+//       },
+//     },
+//   );
+//   const json = await resp.json();
+//   const time = new Date(json.datetime);
+//   return time.toLocaleString("en-US", { timeZone: "PST" }) + " PST";
+// }
 
 export default async function Updated() {
-  const time = await getTime();
+  // const time = await getTime();
   return (
     <div className="bg-green-400 p-1 rounded-md">
-      <span className="font-bold">Last updated:</span> {time}
+      <span className="font-bold">Last updated:</span>{" "}
+      {"(feature broken will fix soon)"}
     </div>
   );
 }
