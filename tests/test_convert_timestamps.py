84a2798d1c9cb50e22c4f58f4614bf73c108a48e
trence <tjtong2006@gmail.com>
2023-09-23 07:11:31 -0700

    added a testcase

--- tests/test_convert_timestamps.py
+++ tests/test_convert_timestamps.py
@@ -1,6 +1,14 @@
-from transform.util import convert_relative_date_to_timestamp
+from transform.util import (
+    convert_relative_date_to_timestamp,
+    convert_time_to_isoformat
+)
 import datetime
 
+def test_convert_time_to_isoformat():
+    time = "9/17/2023 11:46:49 AM"
+    assert (
+        convert_time_to_isoformat(time) == "2023-09-17T11:46:49-07:00"
+    )
 
 def test_convert_relative_date_to_timestamp():
     isotime = convert_relative_date_to_timestamp("1 day ago")
