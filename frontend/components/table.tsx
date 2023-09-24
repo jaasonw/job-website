ea75139ec4ae57ff558bbe7ce09a613cbb169566
jaasonw <jayywong92@gmail.com>
2023-09-23 22:51:40 -0700

    refactor table to not depend on the structure of the csv

--- frontend/components/table.tsx
+++ frontend/components/table.tsx
@@ -1,5 +1,14 @@
-import { UPDATE_INTERVAL } from "$lib/src/constants";
 import Papa from "papaparse";
+
+interface Job {
+  URL: string;
+  Title: string;
+  Company: string;
+  Date: string;
+  Location: string;
+  Technologies: string;
+  "Years of Experience": string;
+}
 
 async function load() {
   const url = process.env.GOOGLE_SHEETS_URL ?? "";
@@ -7,41 +16,52 @@ async function load() {
     cache: "no-store",
   });
   const csv = await resp.text();
-  let parsed = Papa.parse(csv);
+  let parsed = Papa.parse(csv, { header: true });
 
-  // remove 4th and 6th column from data
-  parsed.data = parsed.data.map((col: any) =>
-    col.filter((_: any, i: number) => i !== 4 && i !== 6),
-  );
-  return parsed.data;
+  const data = parsed.data.map((col: any) => {
+    return {
+      Date: col["Date"],
+      URL: col["URL"],
+      Company: col["Company"] || "N/A",
+      Title: col["Title"],
+      Location: col["Location"],
+      Technologies: col["Technologies"],
+      "Years of Experience": col["Years of Experience"],
+    } as Job;
+  });
+  return data;
 }
 
 export default async function JobsTable() {
-  const data = (await load()) as any[];
+  const data = (await load()) as Job[];
+  const keys = Object.keys(data[0]);
   return (
     <div className="w-full h-[50vh] overflow-x-scroll overflow-y-scroll">
       <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
-            {data[0].map((col: any) => (
+            {keys.map((col: any) => (
               <th className="px-6 py-3">{col}</th>
             ))}
           </tr>
         </thead>
         <tbody>
-          {data.slice(1).map((row: any) => (
+          {data.map((row) => (
             <tr>
-              {row.map((col: any, i: number) =>
-                i == 0 ? (
-                  <td className="px-6 py-4">
-                    <a className="underline" href={col} target="_blank">
-                      Apply
-                    </a>
-                  </td>
-                ) : (
-                  <td className="px-6 py-4">{col}</td>
-                ),
-              )}
+              {Object.entries(row).map(([key, value]) => {
+                switch (key) {
+                  case "URL":
+                    return (
+                      <td className="px-6 py-4">
+                        <a className="underline" href={value} target="_blank">
+                          Apply
+                        </a>
+                      </td>
+                    );
+                  default:
+                    return <td className="px-6 py-4">{value}</td>;
+                }
+              })}
             </tr>
           ))}
         </tbody>
